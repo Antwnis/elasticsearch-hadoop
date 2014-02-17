af12dfb92740b36a431569ff9420a57ef1a12e15
Costin Leau <costin.leau@gmail.com>
2014-02-17 23:51:45 +0200

    draft implementation for Cascading Lingual

--- src/main/java/org/elasticsearch/hadoop/cascading/EsTap.java
+++ src/main/java/org/elasticsearch/hadoop/cascading/EsTap.java
@@ -22,9 +22,6 @@ import java.io.IOException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.elasticsearch.hadoop.cfg.Settings;
-import org.elasticsearch.hadoop.cfg.SettingsManager;
-import org.elasticsearch.hadoop.util.StringUtils;
 
 import cascading.flow.Flow;
 import cascading.flow.FlowElement;
@@ -90,9 +87,7 @@ public class EsTap extends Tap<Object, Object, Object> {
     @Override
     public void flowConfInit(Flow<Object> flow) {
         initInnerTapIfNotSet(flow, "cascading.flow.hadoop.HadoopFlow");
-        if (runningInHadoop) {
-            CascadingUtils.addSerializationToken(flow.getConfig());
-        }
+        actualTap.flowConfInit(flow);
     }
 
     @Override
